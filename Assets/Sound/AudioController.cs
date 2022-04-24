e155e5304a2dd6fdd3149c457f62024a362592c2
Nick Thorpe <nickthorpe71@gmail.com>
2022-04-24 08:19:59 -0500

    add working manu music + spell sft and placeholder battle music + ui SFX

--- Assets/Sound/AudioController.cs
+++ Assets/Sound/AudioController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace UnityCore
 {
@@ -6,7 +7,89 @@ namespace UnityCore
     {
         public class AudioController : MonoBehaviour
         {
+            // members
+            public static AudioController instance;
+            public bool debug;
+            public AudioTrack[] tracks;
 
+            private Hashtable m_AudioTable; // relationship between audio types (key) and audio tracks (value)
+            private Hashtable m_JobTable;   // relationship between audio types (key) and jobs (value) (Coroutine, IEnumerator)
+
+            [System.Serializable]
+            public class AudioObject
+            {
+                public AudioType type;
+                public AudioClip clip;
+            }
+
+            [System.Serializable]
+            public class AudioTrack
+            {
+                public AudioSource source;
+                public AudioObject[] audio;
+            }
+
+            #region Unity Functions
+            private void Awake()
+            {
+                Configure();
+            }
+
+            private void OnDisable()
+            {
+                Dispose();
+            }
+
+            #endregion
+
+            #region Public Functions
+            public void Play(AudioType _type)
+            {
+
+            }
+
+            public void Stop(AudioType _type)
+            {
+
+            }
+
+            public void Restart(AudioType _type)
+            {
+
+            }
+
+            #endregion
+
+            #region Private Functions
+            private void Configure()
+            {
+                m_AudioTable = new Hashtable();
+                m_JobTable = new Hashtable();
+                GenerateAudioTable();
+            }
+
+            private void Dispose()
+            {
+
+            }
+
+            private void GenerateAudioTable()
+            {
+
+            }
+
+            private void Log(string _msg)
+            {
+                if (!debug) return;
+                Debug.Log($"[Audio Controller]: {_msg}");
+            }
+            private void LogWarning(string _msg)
+            {
+                if (!debug) return;
+                Debug.LogWarning($"[Audio Controller]: {_msg}");
+            }
+
+            #endregion
         }
     }
 }
--- Assets/Sound/AudioType.cs
+++ Assets/Sound/AudioType.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AudioType : MonoBehaviour
+namespace UnityCore
 {
-    // Start is called before the first frame update
-    void Start()
+    namespace Audio
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public enum AudioType
+        {
+            MUSIC_MAIN_MENU,
+            MUSIC_BATTLE,
+            SFX_BLACK_HIT,
+            SFX_WHITE_HIT,
+            SFX_RED_HIT,
+            SFX_BLUE_HIT,
+            SFX_YELLOW_HIT,
+            SFX_GREEN_HIT,
+            SFX_DEATH,
+            SFX_CAST,
+            SFX_WARP,
+            UI_CLICK,
+            UI_SUBMIT,
+            UI_EXIT,
+            UI_TRANSITION
+        }
     }
 }
+
+
